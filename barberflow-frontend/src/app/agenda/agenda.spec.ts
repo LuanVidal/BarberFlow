ea0f8eb7ce1de0d617bc34fc8a08227e12bce21f
Thiago Mendes Borges <83738447+https-thiagu@users.noreply.github.com>
2025-12-01 10:28:52 -0300

    Update agenda.spec.ts

--- barberflow-frontend/src/app/agenda/agenda.spec.ts
+++ barberflow-frontend/src/app/agenda/agenda.spec.ts
@@ -1,23 +1,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { Agenda } from './agenda';
 
-describe('Agenda', () => {
-  let component: Agenda;
+describe('Agenda Component', () => {
   let fixture: ComponentFixture<Agenda>;
+  let component: Agenda;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Agenda]
-    })
-    .compileComponents();
+      imports: [Agenda] 
+    }).compileComponents();
 
     fixture = TestBed.createComponent(Agenda);
     component = fixture.componentInstance;
-    await fixture.whenStable();
+
+    fixture.detectChanges(); o
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
   });
 });
