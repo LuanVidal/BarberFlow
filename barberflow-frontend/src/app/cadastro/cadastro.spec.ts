91cb67627b5458db19404efe66a8bed0e789a72b
Thiago Mendes Borges <83738447+https-thiagu@users.noreply.github.com>
2025-12-01 10:27:39 -0300

    Update cadastro.spec.ts

--- barberflow-frontend/src/app/cadastro/cadastro.spec.ts
+++ barberflow-frontend/src/app/cadastro/cadastro.spec.ts
@@ -2,22 +2,22 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { Cadastro } from './cadastro';
 
-describe('Cadastro', () => {
-  let component: Cadastro;
+describe('Cadastro Component', () => {
   let fixture: ComponentFixture<Cadastro>;
+  let component: Cadastro;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Cadastro]
-    })
-    .compileComponents();
+      imports: [Cadastro] 
+    }).compileComponents();
 
     fixture = TestBed.createComponent(Cadastro);
     component = fixture.componentInstance;
-    await fixture.whenStable();
+
+    fixture.detectChanges(); 
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
   });
 });
