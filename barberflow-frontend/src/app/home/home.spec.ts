1ec6ba250891023a45c325e34947d838576d7ee1
Thiago Mendes Borges <83738447+https-thiagu@users.noreply.github.com>
2025-12-01 10:17:24 -0300

    Update home.spec.ts

--- barberflow-frontend/src/app/home/home.spec.ts
+++ barberflow-frontend/src/app/home/home.spec.ts
@@ -2,22 +2,21 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { Home } from './home';
 
-describe('Home', () => {
-  let component: Home;
+describe('Home Component', () => {
   let fixture: ComponentFixture<Home>;
+  let component: Home;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [Home]
-    })
-    .compileComponents();
+    }).compileComponents();
 
     fixture = TestBed.createComponent(Home);
     component = fixture.componentInstance;
-    await fixture.whenStable();
+    fixture.detectChanges(); 
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
   });
 });
